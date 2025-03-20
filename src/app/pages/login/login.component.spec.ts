7f92a1f8d710d157bce075955f81c92d132f30dd
Webert Fernandes <106758545+vibrazzi@users.noreply.github.com>
2025-03-20 17:16:53 -0300

    Alterações na responsividade

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
